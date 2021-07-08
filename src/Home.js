a4b8aaabb473caaf5d893e4955a3ec5f9224f635
Zahid Rabbi <alzahidrabbi@gmail.com>
2021-07-09 03:42:02 +0600

    clean code

--- src/Home.js
+++ src/Home.js
@@ -11,6 +11,7 @@ import SettingsIcon from '@material-ui/icons/Settings';
 import CloseIcon from '@material-ui/icons/Close';
 import "./Home.css";
 
+// Style Section ...
 const customStyles = {
     content: {
       top: '50%',
@@ -24,16 +25,37 @@ const customStyles = {
     },
   };
 
-//   const disableDelete = {
-//     backgroundColor:'rgb(128,128,128,0.5)', 
-//     borderRadius:"50%"
-//   };
+  const playIcon = {
+    marginTop:'-70vh',
+    marginLeft: '-58vh', 
+    color:'white', 
+    backgroundColor:'rgb(128,128,128,0.5)',
+    borderRadius:"50%"
+  }
 
-//   const activeDelete = {
-//         color: 'white',
-//         backgroundColor:'rgb(128,128,128,0.5)', 
-//         borderRadius:"50%"
-//   }
+//   style={{marginTop:'-70vh', marginLeft: '-58vh', color:'white', backgroundColor:'rgb(128,128,128,0.5)', borderRadius:"50%"}}
+
+  const settingIcon = {
+    color:'white', 
+    backgroundColor:'rgb(128,128,128,0.5)', 
+    borderRadius:"50%", 
+    padding:'10px', 
+    marginTop:'-750px', 
+    marginLeft:'790px'
+  }
+
+//   color:'white', backgroundColor:'rgb(128,128,128,0.5)', borderRadius:"50%", padding:'10px', marginTop:'-750px', marginLeft:'790px'
+
+  const pausedIcon = {
+    color: 'red', 
+    backgroundColor: 'white', 
+    borderRadius:'50%', 
+    padding:'10px', 
+    width:'40px', 
+    height:'40px'
+  }
+
+//   { color: 'red', backgroundColor: 'white', borderRadius:'50%', padding:'10px', width:'40px', height:'40px'}
 
   Modal.setAppElement('#root');
 
@@ -42,17 +64,17 @@ function Home() {
     const [ record, setRecord ] = useState(true);
     const [ afterRecord, setAfterRecord ] = useState(true);
 
-    let subtitle;
+    // let subtitle;
   const [modalIsOpen, setIsOpen] = React.useState(false);
 
   function openModal() {
     setIsOpen(true);
   }
 
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    // subtitle.style.color = '#f00';
-  }
+//   function afterOpenModal() {
+//     // references are now sync'd and can be accessed.
+//     // subtitle.style.color = '#f00';
+//   }
 
   function closeModal() {
     setIsOpen(false);
@@ -60,76 +82,64 @@ function Home() {
 
     return (
         <div>
-      <button className="btn btn-danger mt-5" onClick={openModal}>Talk Mail</button>
-      <Modal
-        isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
-        onRequestClose={closeModal}
-        style={customStyles}
-        contentLabel="Example Modal"
-      >        
-        {/* Start from here...  */}
-        <video style={{width:"320", height:"240", borderRadius:"10px"}} controls>  
-            <source src={video} type="video/ogg"/>                         
-        </video>
-        {!afterRecord && 
-                <IconButton className="play-icon" style={{marginTop:'-70vh', marginLeft: '-58vh', color:'white', backgroundColor:'rgb(128,128,128,0.5)', borderRadius:"50%"}}>            
+            <button className="btn btn-danger mt-5" onClick={openModal}>Talk Mail</button>
+            <Modal
+                isOpen={modalIsOpen}
+                // onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+            >        
+                <video className="vdo-style" controls>  
+                    <source src={video} type="video/ogg"/>                         
+                </video>
+                    
+                {!afterRecord && 
+                <IconButton className="play-icon" style={playIcon}>            
                     <PlayArrowIcon style={{color:'white'}}/>
                 </IconButton>}
-        <div style={{marginTop: "-150px"}} className="d-flex justify-content-center">                  
+                
+                <div style={{marginTop: "-150px"}} className="d-flex justify-content-center">
+                    <div style={{color:'white', position: 'absolute',marginTop: "-25px"}}>
+                        {!record &&<p><small><FiberManualRecordIcon className="timerBall" /></small>00:04:27</p>}
+                    </div>
 
-                {/* {!afterRecord && <PlayArrowIcon style={{color:'white'}}/>} */}
+                    <div>
+                        {afterRecord?
+                        <IconButton  className="m-1" style={{ backgroundColor:'rgb(128,128,128,0.5)', borderRadius:"50%"}}>
+                            <DeleteIcon/>
+                        </IconButton>:
+                        <IconButton className="m-1" style={{color:'white', backgroundColor:'rgb(128,128,128,0.5)', borderRadius:"50%"}} onClick={()=>setAfterRecord(true)}>
+                            <DeleteIcon/>
+                        </IconButton>}
 
-            <div style={{color:'white', position: 'absolute',marginTop: "-25px"}}>
-                {!record &&<p><small><FiberManualRecordIcon className="timerBall" /></small>00:04:27</p>}
-            </div>
-            <div>
-                {afterRecord?
-                <IconButton  className="m-1" style={{ backgroundColor:'rgb(128,128,128,0.5)', borderRadius:"50%"}}>
-                    <DeleteIcon/>
-                </IconButton>:
-                <IconButton className="m-1" style={{color:'white', backgroundColor:'rgb(128,128,128,0.5)', borderRadius:"50%"}} onClick={()=>setAfterRecord(true)}>
-                    <DeleteIcon/>
-                </IconButton>}
+                        {record?
+                        <IconButton onClick={()=>setRecord(false)} style={{backgroundColor:'rgb(128,128,128,0.5)', borderRadius:"50%"}}>            
+                            <FiberManualRecordIcon style={{ color: 'red', width:'40px', height:'40px'}}/>
+                        </IconButton>:
+                        <IconButton onClick={()=>{setRecord(true); setAfterRecord(false)}} style={{backgroundColor:'rgb(128,128,128,0.5)', borderRadius:"50%"}}>            
+                            <PausePresentationIcon style={pausedIcon}/>
+                        </IconButton>}
 
-                {record?
-                <IconButton onClick={()=>setRecord(false)} style={{backgroundColor:'rgb(128,128,128,0.5)', borderRadius:"50%"}}>            
-                    <FiberManualRecordIcon style={{ color: 'red', width:'40px', height:'40px'}}/>
-                </IconButton>:
-                <IconButton onClick={()=>{setRecord(true); setAfterRecord(false)}} style={{backgroundColor:'rgb(128,128,128,0.5)', borderRadius:"50%"}}>            
-                    <PausePresentationIcon style={{ color: 'red', backgroundColor: 'white', borderRadius:'50%', padding:'10px', width:'40px', height:'40px'}}/>
-                </IconButton>}
+                        {afterRecord?
+                        <IconButton className="m-1" style={{backgroundColor:'rgb(128,128,128,0.5)', borderRadius:"50%"}}>            
+                            <SendIcon/>
+                        </IconButton>:
+                        <IconButton className="m-1" style={{color:'white', backgroundColor:'rgb(128,128,128,0.5)', borderRadius:"50%"}}>            
+                            <SendIcon/>
+                        </IconButton>}        
+                    </div>
+                </div>
 
-                {afterRecord?
-                <IconButton className="m-1" style={{backgroundColor:'rgb(128,128,128,0.5)', borderRadius:"50%"}}>            
-                    <SendIcon/>
-                </IconButton>:
-                <IconButton className="m-1" style={{color:'white', backgroundColor:'rgb(128,128,128,0.5)', borderRadius:"50%"}}>            
-                    <SendIcon/>
-                </IconButton>}
+                    <IconButton style={settingIcon}>            
+                        <SettingsIcon/>
+                    </IconButton>
 
-                            
-            </div>
+                    <IconButton onClick={()=>closeModal()} style={{marginTop:'-850px'}}>
+                        <CloseIcon/>
+                    </IconButton>
+            </Modal>      
         </div>
-
-        {/* {!afterRecord && 
-        <IconButton className="play-icon" style={{marginTop:'-60vh', marginLeft: '47%',color:'white', backgroundColor:'rgb(128,128,128,0.5)', borderRadius:"50%"}}>            
-            <PlayArrowIcon style={{color:'white'}}/>
-        </IconButton>} */}
-        
-            <IconButton style={{color:'white', backgroundColor:'rgb(128,128,128,0.5)', borderRadius:"50%", padding:'10px', marginTop:'-750px', marginLeft:'790px'}}>            
-                <SettingsIcon/>
-            </IconButton>
-
-            <IconButton onClick={()=>closeModal()} style={{marginTop:'-850px'}}>
-                <CloseIcon/>
-            </IconButton>
-            
-        
-        
-        
-      </Modal>      
-    </div>
     )
 }
 
